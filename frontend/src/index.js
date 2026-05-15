ec8e19ce40e378b93dce03ffdfa1b34cf3c9dd5a
JohnathanGould <johnathangould@gmail.com>
2026-05-15 17:17:11 -0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,10 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.1,
+});
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
